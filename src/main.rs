427ccf62d2196dc7fc3ecde0058b628240b9f506
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-09-21 15:45:42 +0200

    Add can

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use futures_util::stream::StreamExt;
+use tokio_socketcan::{CANSocket, Error};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut socket_rx = CANSocket::open("can0")?;
+
+    while let Some(Ok(frame)) = socket_rx.next().await {
+        let data = frame.data();
+        let id = frame.id();
+
+    }
+    Ok(())
 }
