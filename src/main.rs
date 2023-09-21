2723065f603a4d23cbaa53ef2a40a1888adf1720
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-09-21 15:51:25 +0200

    Add log

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use futures_util::stream::StreamExt;
 use tokio_socketcan::{CANSocket, Error};
 
+#[derive(Default)]
+struct Stats;
+
+impl Stats {
+    fn push(&mut self, frame: tokio_socketcan::CANFrame) {
+        log::info!("{:?}", &frame);
+        let _data = frame.data();
+        let _id = frame.id();
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    env_logger::init();
+
     let mut socket_rx = CANSocket::open("can0")?;
+    let mut stats = Stats::default();
 
     while let Some(Ok(frame)) = socket_rx.next().await {
-        let data = frame.data();
-        let id = frame.id();
-
+        stats.push(frame);
     }
     Ok(())
 }
