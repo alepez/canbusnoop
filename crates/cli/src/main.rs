3372c3a5302afa94967364814b79b54dc64deac1
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-12-18 22:26:27 +0100

    WIP Integrate can bus receiver with ui

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,8 +1,10 @@
 #![allow(dead_code)]
 
 use canbusnoop_bus::{CanBusReader, Config};
+use canbusnoop_core::Frame;
 use canbusnoop_db::*;
 use canbusnoop_ui::launch;
+use futures_channel::mpsc::{unbounded, UnboundedSender};
 
 #[tokio::main]
 async fn _main() -> Result<(), Box<dyn std::error::Error>> {
@@ -21,9 +23,35 @@ async fn _main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+fn sniff_background_task(
+    rx_sender: UnboundedSender<Frame>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // let config = Config::new_socket_can("can0".to_string());
+    // let mut reader = CanBusReader::new(config)?;
+    //
+    // while let Some(frame) = reader.read().await {
+    //     rx_sender.unbounded_send(frame)?;
+    // }
+
+    loop {
+        let frame = Frame::new(0x123, vec![0x01, 0x02, 0x03, 0x04, 0x05, 0x06]);
+        rx_sender.unbounded_send(frame)?;
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_env_logger();
-    launch();
+
+    let (rx_sender, rx_receiver) = unbounded::<Frame>();
+
+    std::thread::spawn(move || {
+        let _ = sniff_background_task(rx_sender);
+    });
+
+    launch(rx_receiver);
 
     Ok(())
 }
--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,22 +1,34 @@
 #![allow(non_snake_case)]
-use dioxus::prelude::*;
 
-pub fn launch() {
-    dioxus_desktop::launch(App);
+use canbusnoop_core::Frame;
+use dioxus::prelude::*;
+use dioxus_desktop::Config;
+use futures::StreamExt;
+use log::info;
+use std::cell::Cell;
+
+struct AppProps {
+    rx_receiver: Cell<Option<UnboundedReceiver<Frame>>>,
 }
 
-fn App(cx: Scope) -> Element {
-    let mut started = use_state(cx, || false);
+pub fn launch(rx_receiver: UnboundedReceiver<Frame>) {
+    let rx_receiver = Cell::new(Some(rx_receiver));
+    let props = AppProps { rx_receiver };
+    let config = Config::default();
+    dioxus_desktop::launch_with_props(App, props, config);
+}
 
-    if !started {
-        started.set(true);
-        let _ = tokio::spawn(async {
-            let _ = tokio::task::spawn_local(async {
-                // some !Send work
-            })
-            .await;
-        });
-    }
+fn App(cx: Scope<AppProps>) -> Element {
+    let _ = use_coroutine(cx, |_: UnboundedReceiver<()>| {
+        let receiver = cx.props.rx_receiver.take();
+        async move {
+            if let Some(mut receiver) = receiver {
+                while let Some(msg) = receiver.next().await {
+                    info!("Received: {:?}", msg);
+                }
+            }
+        }
+    });
 
     cx.render(rsx! {
         div {
