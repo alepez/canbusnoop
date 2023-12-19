166fb14ec1cb968394e5cb3306c491affd1b42f4
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-12-19 23:45:09 +0100

    Fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./crates/ui/src/**/*.{rs,html,css}",
     "./src/**/*.{rs,html,css}",
     "./dist/**/*.html",
   ],
